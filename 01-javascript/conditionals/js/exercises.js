71fda38c54ea4b59710ce4c480b0f059e0185b7c
wofockham <certainthings@gmail.com>
2021-06-23 14:50:27 +1000

    conditional solutions

--- 01-javascript/conditionals/js/exercises.js
+++ 01-javascript/conditionals/js/exercises.js
@@ -48,7 +48,28 @@ console.log( helloWorld('ko') );
 // - takes 1 argument, a number score.
 // - returns a grade for the score, either "A", "B", "C", "D", or "F".
 // - Call that function for a few different scores and log the result to make sure it works.
-//
+
+const assignGrade = function (score) {
+  if (score >= 80) {
+    return 'A';
+  } else if (score >= 70) {
+    return 'B';
+  } else if (score >= 60) {
+    return 'C';
+  } else if (score >= 50) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+};
+
+console.log( assignGrade(15) );
+console.log( assignGrade(51) );
+console.log( assignGrade(61) );
+console.log( assignGrade(71) );
+console.log( assignGrade(81) );
+console.log( assignGrade(100) );
+
 // ## The Pluralizer
 //
 // Write a function named pluralize that:
@@ -56,3 +77,16 @@ console.log( helloWorld('ko') );
 // - returns the number and pluralized form, like "5 cats" or "1 dog".
 // - Call that function for a few different scores and log the result to make sure it works.
 // - Bonus: Make it handle a few collective nouns like "sheep" and "geese".
+
+const pluralize = function (noun, number) {
+  const message = `${ number } ${ noun }`;
+  if (1 === number) {
+    return message;
+  } else {
+    return message + 's';
+  }
+}
+
+console.log( pluralize('dog', 1) );
+console.log( pluralize('cat', 5) );
+console.log( pluralize('turtle', 0) );
