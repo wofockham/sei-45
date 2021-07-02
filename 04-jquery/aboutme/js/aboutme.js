4f523f4030d3767ad763c67ed4c2c19b84799c6e
wofockham <certainthings@gmail.com>
2021-07-02 10:48:56 +1000

    jquery about me

---
+++ 04-jquery/aboutme/js/aboutme.js
@@ -0,0 +1,12 @@
+
+$('body').css('font-family', 'Arial, sans serif');
+
+const $nickname = $('#nickname');
+$nickname.html('The Blade'); // setter
+
+$('#favorites').html('cheesecake, butterflies, hotdogs');
+$('#hometown').html('Sydney');
+
+$('li').addClass('listitem');
+
+$('<img src="http://www.fillmurray.com/120/300">').appendTo('body');
