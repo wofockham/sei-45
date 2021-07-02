301b8613a8d8ce0659096a4de4a92cb3c767e25b
wofockham <certainthings@gmail.com>
2021-07-02 10:48:47 +1000

    jquery madlibs

---
+++ 04-jquery/madlibs/js/madlibs.js
@@ -0,0 +1,14 @@
+
+const makeMadLib = function () {
+  const noun = $('#noun').val(); // string
+  const adjective = $('#adjective').val();
+  const name = $('#person').val();
+
+  const story = `${ name } really likes ${ adjective } ${ noun }.`;
+  $('#story').append(`<p>${ story }</p>`);
+
+};
+
+$('#lib-button').on('click', makeMadLib);
+
+$('h1').funText(100, ['#ff7400', '#cd0074', '#0c0']);
