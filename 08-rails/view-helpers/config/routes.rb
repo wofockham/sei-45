89b95a60a3013c1dea128f86be01d94da9285bea
wofockham <certainthings@gmail.com>
2021-07-20 15:33:48 +1000

    URL helpers

--- 08-rails/view-helpers/config/routes.rb
+++ 08-rails/view-helpers/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
 
-  get '/numbers' => 'pages#numbers'
+  get '/kfc-numbers' => 'pages#numbers', :as => :numbers
   get '/text' => 'pages#text'
   get '/assets' => 'pages#assets'
   get '/url' => 'pages#url'
