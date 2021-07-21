e5f10d2472bf38334c2bc67127f82d9de0efaf4a
wofockham <certainthings@gmail.com>
2021-07-21 14:52:17 +1000

    works CRUD

---
+++ 08-rails/moma/app/controllers/works_controller.rb
@@ -0,0 +1,40 @@
+class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.create work_params
+    redirect_to work # GET show
+  end
+
+  def edit
+    @work = Work.find params[:id]
+  end
+
+  def update
+    work = Work.find params[:id]
+    work.update work_params
+    redirect_to work # GET show
+  end
+
+  def show
+    @work = Work.find params[:id]
+  end
+
+  def destroy
+    work = Work.find params[:id]
+    work.destroy
+    redirect_to works_path
+  end
+
+  # Strong Params
+  private
+  def work_params
+    params.require(:work).permit(:title, :year, :medium, :style, :image)
+  end
+end
