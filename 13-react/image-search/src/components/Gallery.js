172dd6cce0e0f3b63ed5f97c0ec64cae10978a18
wofockham <certainthings@gmail.com>
2021-08-09 15:34:43 +1000

    Gallery functional component

--- 13-react/image-search/src/components/Gallery.js
+++ 13-react/image-search/src/components/Gallery.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Gallery extends Component {
-  render() {
-    return (
-      <div>
-        { this.props.images.map((url) => <img src={url} />)}
-      </div>
-    )
-  }
+// Functional components: good when you don't need state.
+// Think of it as just the render method.
+// No `this`.
+
+// Access props by creating a parameter for them.
+const Gallery = (props) => {
+  return (
+    <div>
+      { props.images.map( (url) => (<img src={ url } key={ url } alt="Copyright Flickr" />) ) }
+    </div>
+  );
 }
 
 export default Gallery;
