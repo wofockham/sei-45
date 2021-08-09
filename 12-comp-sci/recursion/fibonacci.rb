1855abc61c031c39c457d7646478fc7fba0f412e
wofockham <certainthings@gmail.com>
2021-08-09 10:50:26 +1000

    iterative and memoised recursion

--- 12-comp-sci/recursion/fibonacci.rb
+++ 12-comp-sci/recursion/fibonacci.rb
@@ -17,11 +17,39 @@ def fibonacci_iterative(n)
   b
 end
 
-def fibonacci_recursive(n)
+def fibonacci_recursive(n) # O(2^n)
   if n <= 2
     1
   else
     fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+  end
+end
+
+# Bonus homework:
+# Make this faster but still recursive.
+
+# Option a: memoisation
+# Option b: iterative recursion: only recurse ONCE per call
+
+def fib_memo(n)
+  @fib ||= {} # assign an empty hash to @fib if it doesn't already have a value
+
+  if @fib[n]
+    @fib[n]
+  elsif n <= 2
+    1
+  else
+    @fib[n] = fib_memo(n-1) + fib_memo(n-2)
+  end
+end
+
+# Iterative recursion
+def fib(n, a=1, b=1)
+  # puts "n: #{ n }, a: #{ a }, b: #{ b }"
+  if n == 1 || n == 2
+    b
+  else
+    fib n-1, b, a+b # mystical
   end
 end
 
