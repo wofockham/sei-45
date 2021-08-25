0102877b393ea147208f8ad3b48a1c671669eac4
wofockham <certainthings@gmail.com>
2021-08-25 16:32:35 +1000

    express wall street stocks

---
+++ 15-node/stocks/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ejs = require('ejs');
+const stockfinder = require('stockfinder');
+
+const server = express();
+const PORT = 1337; // could be any number higher than 1024
+const APIKEY = 'dingoes';
+
+server.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+server.get('/info', (req, res) => {
+  stockfinder.getStock({ticker: req.query.symbol, apiKey: APIKEY}).then((result) => {
+    res.render('info.ejs', { price: result.iexRealtimePrice, symbol: req.query.symbol });
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running: http://localhost:${ PORT }`));
