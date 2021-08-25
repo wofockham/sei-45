21d41c84308505a16738fbfca9d41b8133078dd2
wofockham <certainthings@gmail.com>
2021-08-25 15:05:25 +1000

    express intro ii

--- 15-node/express-intro/index.js
+++ 15-node/express-intro/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
-
-// TODO:
-// view files
+const _ = require('underscore');
+const axios = require('axios'); // fetch
 
 // Basic setup
 const server = express();
@@ -10,7 +9,9 @@ server.set('view-engine', ejs);
 const PORT = 1337;
 
 server.get('/', (req, res) => {
-  res.render('home.ejs');
+  const bros = 'Groucho Chico Harpo Zeppo'.split(' ');
+  const randomBrother = _(bros).sample();
+  res.render('home.ejs', {brother: randomBrother, lucky: _.random(10)}); // "Instance variables"
 });
 
 server.get('/hello', (req, res) => {
@@ -22,5 +23,42 @@ server.get('/fanclub/:name', (req, res) => {
   res.send(`${ req.params.name } is a member of the Marx Brothers fan club`);
 });
 
+// Crappy calculator: /calc/5/6/+
+server.get('/calc/:x/:y/:operator', (req, res) => {
+  let answer;
+  console.log( JSON.stringify(req.params) );
+  if (req.params.operator === '+') {
+    answer = Number(req.params.x) + Number(req.params.y); // bug here
+  } else if (req.params.operator === '-') {
+    answer = Number(req.params.x) - Number(req.params.y);
+  } // todo: handle multiplication and division
+  res.render('calc.ejs', { answer: answer });
+});
+
+// Render JSON
+server.get('/info.json', (req, res) => {
+  res.json({
+    name: 'Groucho',
+    instrument: 'guitar',
+    vice: 'cigars',
+    luckyNumber: _.random(40)
+  });
+});
+
+// Something like HTTParty to talk to APIs
+server.get('/trivia', (req, res) => {
+  axios.get('http://numbersapi.com/random/trivia').then((response) => {
+    res.render('trivia.ejs', { trivia: response.data });
+  });
+});
+
+// Form input/output
+server.get('/ask', (req, res) => {
+  res.render('ask.ejs'); // form
+});
+
+server.get('/answer', (req, res) => {
+  res.render('answer.ejs', {question: req.query.question, answer: 'No comment'});
+});
 
 server.listen(PORT, () => console.log(`Now serving on http://localhost:${ PORT }`));
